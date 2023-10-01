b61b5013180039459880faac4aeb1d0349eec4d9
akshaikh198 <113336618+akshaikh198@users.noreply.github.com>
2023-10-01 21:39:07 +0530

    Add project files.

---
+++ Insta Mart/Controllers/AdminController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Insta_Mart.Models;
+
+namespace Insta_Mart.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        InstaMartDBEntities db = new InstaMartDBEntities();
+        public ActionResult Index()
+        {
+
+            if(Session["aid"] !=null)
+            {
+            }
+            else
+            {
+                Response.Write("<script>alert('Login First then you can enter Admin Zone');window.location.href='/Home/Login'</script>");
+
+            }
+            return View();
+        }
+
+        public ActionResult ContactMgmt()
+        {
+
+            return View(db.contacts.ToList());
+        }
+        public void Logout()
+        {
+            Session.Abandon();
+            Response.Write("<script>alert('Logout');window.location.href='/Home/Login'</script>");
+        }
+
+        public ActionResult ChangePassword()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public void ChangePassword(string oldpass, string newpass, string confirmpass)
+        {
+            try
+            {
+                if (newpass == confirmpass)
+                {
+                    signIn lg = db.signIns.Where(x => x.password == oldpass).SingleOrDefault();
+                    lg.password = confirmpass;
+                    db.SaveChanges();
+                    Response.Write("<script>alert('Your Password Changed Successfully');window.location.href='/Home/Login'</script>");
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('Your Password Does Not Match');window.location.href='/Home/Login'</script>");
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('We cannot Change Your Password');window.location.href='/Home/Login'</script>");
+            
+            }
+            
+        }
+
+    }
+}
---
+++ Insta Mart/Controllers/HomeController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Insta_Mart.Models;
+
+namespace Insta_Mart.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        InstaMartDBEntities db = new InstaMartDBEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+        return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public ActionResult Contact(contact Reg, string cp1, string hd1)
+        {
+            try {
+                if (hd1 == cp1)
+                {
+                    db.contacts.Add(Reg);
+                    db.SaveChanges();
+                    Response.Write("<script>alert('massage sent Successfully !')</script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Captcha Code !')</script>");
+                }
+            }
+
+            catch(Exception ex)
+            {
+             Response.Write("<script>alert('Sorry Please Try Again Later !')</script>");
+            }
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(signUp rg)
+        {
+            try
+            {
+                db.SaveChanges();
+                db.signUps.Add(rg);
+                Response.Write("<script>window.location.href='/Home/Successfull'</script>");
+
+            }
+
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('Please Again Later !!');window.location.href='/Home/Registration'</script>");
+            }
+            return View();
+        }
+
+        public ActionResult Successfull()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public ActionResult Login(signIn lg)
+        {
+            try
+            { 
+            var res=db.signIns.Where(x=> x.email==lg.email && x.password==lg.password).SingleOrDefault();
+                if(res.email==lg.email && res.password==lg.password)
+                {
+                    Session["aid"] = res.email;
+                Response.Write("<script>alert('Welcome to Admin Zone');window.location.href='/Admin/Index'</script>");
+                }
+
+                else
+                    {
+                Response.Write("<script>alert('Invalid User Id Or Password')</script>");
+                }
+            
+            }
+
+            catch(Exception ex)
+            {
+            
+             Response.Write("<script>alert('Invalid User Id Or Password')</script>");
+            }
+            return View();
+        }
+    }
+}
